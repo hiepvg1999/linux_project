6972acac88f866d479460c98a360e6b9b659b401
hiepvg1999 <hiepbkcntt@gmail.com>
2020-11-03 09:21:11 +0700

    Update sort by key

--- test.sh
+++ test.sh
@@ -21,7 +21,7 @@ check_dependencies
 while getopts ":a:c:l:s:p:h:" opt; do
   case $opt in
     a)
-      if [[ $opt -eq "all" ]]; then
+      if [[ "$2" == "all" ]]; then
         data=$(curl -X GET "https://disease.sh/v3/covid-19/all" -H "accept: application/json")
         cases=$(echo $data |  jq ".cases")
         deaths=$(echo $data | jq ".deaths")
@@ -41,11 +41,12 @@ while getopts ":a:c:l:s:p:h:" opt; do
         echo -e "\e[1;35m todayCases: ${todayCases} \e[0m" >&2
         echo -e "\e[2;36m todayDeaths: ${todayDeaths} \e[0m" >&2
       else
-        echo "Option -$OPTARG wrong!" >&2
+        echo "Wrong argument.Please input (all)" >&2
+        safe_exit
       fi
       ;;
     c)
-        data=$(curl -X GET "https://disease.sh/v3/covid-19/countries/$OPTARG?strict=true" -H "accept: application/json")
+        data=$(curl -X GET "https://disease.sh/v3/covid-19/countries/$2?strict=true" -H "accept: application/json")
         if [[ -z data ]]; then
           echo "Not found data of $-OPTARG country" >&2
         else
@@ -92,46 +93,88 @@ while getopts ":a:c:l:s:p:h:" opt; do
       ;;
     p)
     # sort by keys (cases,deaths,active)
-    if [[ $opt -eq "deaths" ]]; then
-      data=$(curl -X GET "https://disease.sh/v3/covid-19/countries?sort=deaths" -H "accept: application/json")
-    elif [[ $opt -eq "cases" ]]; then
-      data=$(curl -X GET "https://disease.sh/v3/covid-19/countries?sort=cases" -H "accept: application/json")
-    elif [[ $opt -eq "active" ]]; then
-      data=$(curl -X GET "https://disease.sh/v3/covid-19/countries?sort=active" -H "accept: application/json")
-    fi
-    len=$(echo $data | jq length)
-    echo "Number of countries: $len" >&2
-    echo "" | nawk '{printf("========================================================\n");
-      printf("|%20s|%10s|%10s|%10s|\n","country","cases","deaths","active");
-      }'
-    for (( i = 0; i < 5; i++ )); do
-      cases=$(echo $data |  jq -r ".[$i].cases")
-      country=$(echo $data | jq -r ".[$i].country")
-      deaths=$(echo $data | jq -r ".[$i].deaths")
-      active=$(echo $data | jq -r ".[$i].active")
-      echo "${country} ${cases} ${deaths} ${active}" | nawk '{printf("========================================================\n");
-      printf("|%20s|%10s|%10s|%10s|\n",$1,$2,$3,$4);
-      }'
-    done
+    case $2 in
+      deaths)
+        echo "$2"
+        data=$(curl -X GET "https://disease.sh/v3/covid-19/countries?sort=$2" -H "accept: application/json")
+        len=$(echo $data | jq length)
+        echo "Number of countries: $len" >&2
+        echo "" | nawk '{printf("========================================================\n");
+          printf("|%20s|%10s|%10s|%10s|\n","country","cases","deaths","active");
+          }'
+        for (( i = 0; i < 5; i++ )); do
+          cases=$(echo $data |  jq -r ".[$i].cases")
+          country=$(echo $data | jq -r ".[$i].country")
+          deaths=$(echo $data | jq -r ".[$i].deaths")
+          active=$(echo $data | jq -r ".[$i].active")
+          echo "${country} ${cases} ${deaths} ${active}" | nawk '{printf("========================================================\n");
+            printf("|%20s|%10s|%10s|%10s|\n",$1,$2,$3,$4);
+            }'
+        done
+        break  
+        ;;
+      cases)
+        data=$(curl -X GET "https://disease.sh/v3/covid-19/countries?sort=$2" -H "accept: application/json")
+        len=$(echo $data | jq length)
+        echo "Number of countries: $len" >&2
+        echo "" | nawk '{printf("========================================================\n");
+          printf("|%20s|%10s|%10s|%10s|\n","country","cases","deaths","active");
+          }'
+        for (( i = 0; i < 5; i++ )); do
+          cases=$(echo $data |  jq -r ".[$i].cases")
+          country=$(echo $data | jq -r ".[$i].country")
+          deaths=$(echo $data | jq -r ".[$i].deaths")
+          active=$(echo $data | jq -r ".[$i].active")
+          echo "${country} ${cases} ${deaths} ${active}" | nawk '{printf("========================================================\n");
+            printf("|%20s|%10s|%10s|%10s|\n",$1,$2,$3,$4);
+            }'
+        done
+        break
+        ;;
+      active)
+        data=$(curl -X GET "https://disease.sh/v3/covid-19/countries?sort=$2" -H "accept: application/json")
+        len=$(echo $data | jq length)
+        echo "Number of countries: $len" >&2
+        echo "" | nawk '{printf("========================================================\n");
+          printf("|%20s|%10s|%10s|%10s|\n","country","cases","deaths","active");
+          }'
+        for (( i = 0; i < 5; i++ )); do
+          cases=$(echo $data |  jq -r ".[$i].cases")
+          country=$(echo $data | jq -r ".[$i].country")
+          deaths=$(echo $data | jq -r ".[$i].deaths")
+          active=$(echo $data | jq -r ".[$i].active")
+          echo "${country} ${cases} ${deaths} ${active}" | nawk '{printf("========================================================\n");
+            printf("|%20s|%10s|%10s|%10s|\n",$1,$2,$3,$4);
+            }'
+        done
+        break
+        ;;
+      *)
+        echo "Wrong argument.Please input (cases,deaths,active)"
+        safe_exit    
+    esac
       ;;
     h)
     ### menu
-    if [[ $opt -eq "menu" ]]; then
+    if [[ "$2" == "menu" ]]; then
+      echo "$2"
       echo -e "\e[1;44m Options\e[0m" >&2
       echo -e "\e[1;37m -c --country Specific country" >&2
       echo -e "\e[1;37m -a --all All data " >&2
       echo -e "\e[1;37m -c --list List of country id code" >&2
       echo -e "\e[1;37m -s --search Search info about specific country" >&2
-      exit 1
+      safe_exit
+    else
+      echo "Wrong argument.Please input (menu)"
     fi
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
-      exit 1
+      safe_exit
       ;;
     :)
       echo "Option -$OPTARG requires an argument." >&2
-      exit 1
+      safe_exit
       ;;
   esac
 done
